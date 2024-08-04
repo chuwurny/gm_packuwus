539980d86b8121584aa97f6d1465b6243f9acad2
chuwurny <chuwurny@gmail.com>
2024-08-05 02:00:28 +0300

    Fatal error if 'gamemode' table is not loaded

--- lua/includes/init.lua
+++ lua/includes/init.lua
@@ -4,6 +4,13 @@ include("packuwus/sh_main.lua")
 AddCSLuaFile("includes/_init.lua")
 include("includes/_init.lua")
 
+if not gamemode then
+    return PackUwUs.FatalError(PackUwUs.Lang({
+        en = "'_G.gamemode' is nil! Looks like serverside pack error!",
+        ru = "'_G.gamemode' это nil! Похоже ошибка при запаковке на стороне сервера!",
+    }))
+end
+
 if CLIENT then
     include("packuwus/cl_debug_helpers.lua")
 else
